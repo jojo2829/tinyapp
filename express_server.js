033a876471a34171d4e4ec9887aeafa208aaea89
jojo2829 <joulinli@gmail.com>
2021-06-08 02:33:50 +0000

    added 404 if short url doesn't exist

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 
-const urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -14,8 +14,7 @@ function generateRandomString() {
   for (let i = 0; i < 6; i++ ) {
     result += characters.charAt(Math.random() * characters.length);
   }
-  console.log("result: ",result);
-
+  return result;
 }
 
 generateRandomString();
@@ -37,22 +36,34 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-
+//form to enter long url
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//response to submit form (submit long url)
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const shortURL = req.params.shortURL;
+  if (!urlDatabase[shortURL]) {
+    res.status(404).send('Page Not Found');
+  }
+  const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
 
 
 app.listen(PORT, () => {
